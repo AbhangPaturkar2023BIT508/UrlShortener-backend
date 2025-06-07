8d70c4bdbd8b17b6a0ed47988687f4f8658717dd
Abhang Paturkar <155937375+AbhangPaturkar2023BIT508@users.noreply.github.com>
2025-06-07 15:03:00 +0530

    Changes

--- src/main/java/com/urlshortner/Controller/LinkRedirectApi.java
+++ src/main/java/com/urlshortner/Controller/LinkRedirectApi.java
@@ -8,17 +8,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.urlshortner.Services.LinkService;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/r")
 public class LinkRedirectApi {
     @Autowired
     private LinkService linkService;
 
-    private static final String FRONTEND_BASE_URL = "http://localhost:5173";
+    // private static final String FRONTEND_BASE_URL = "http://localhost:5173";
+
+    private static final String FRONTEND_BASE_URL = "https://shortlink-xdxw.onrender.com";
 
     @GetMapping("/{code}")
     public ResponseEntity<Void> redirectToOriginal(@PathVariable String code) {
